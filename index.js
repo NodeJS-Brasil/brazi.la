a7a6755a2e980846c27710f566b9296b59cd5d71
Luan Muniz <luan@luanmuniz.com.br>
2013-12-18 14:11:04 -0200

    Init dev

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 
 var config = require('./config'),
-	core = require('./system/core');
+	core = require('./system/core'),
+	validConfig = core.validateConfigs(config);
 
-core(config);
+if(validConfig !== true){
+	core.init(config);
+} else {
+	console.log(validConfig);
+}
